9bf6a246990842bd9ac93a5e32e46e012be81773
Ivan Rusakov <rrusakovivan@gmail.com>
2026-04-20 20:38:21 +0300

    Initial solution structure

---
+++ RecipePlanner/RecipePlanner.Tests/UnitTest1.cs
@@ -0,0 +1,16 @@
+﻿namespace RecipePlanner.Tests
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
