9d125b149b9338f83c88cbb80eb4744e7d13faed
Ivan Rusakov <rrusakovivan@gmail.com>
2026-05-09 23:20:11 +0300

    Implemented JSON data storage

---
+++ RecipePlanner/RecipePlanner/Core/Interfaces/IRepository.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RecipePlanner.Core.Interfaces
+{
+    internal class IRepository
+    {
+    }
+}
---
+++ RecipePlanner/RecipePlanner/Data/JsonRepository.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RecipePlanner.Data
+{
+    internal class JsonRepository
+    {
+    }
+}
