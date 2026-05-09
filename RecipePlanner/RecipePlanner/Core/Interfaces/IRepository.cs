f63c43f1a26304d96d6e86c3d7f8e127d22fd2fc
Ivan Rusakov <rrusakovivan@gmail.com>
2026-05-09 23:45:17 +0300

    fix: update IRepository interface and models

--- RecipePlanner/RecipePlanner/Core/Interfaces/IRepository.cs
+++ RecipePlanner/RecipePlanner/Core/Interfaces/IRepository.cs
@@ -1,10 +1,14 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+namespace RecipePlanner.Core.Interfaces;
 
-namespace RecipePlanner.Core.Interfaces
+public interface IRepository<T> where T : class
 {
-    internal class IRepository
-    {
-    }
+    IEnumerable<T> GetAll();
+    T GetById(Guid id);
+    void Add(T item);
+    void Update(T item);
+    void Delete(Guid id);
+    void Save();
 }
